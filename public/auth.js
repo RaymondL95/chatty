4f0d09e00b38459c7cedc80f83a65a36c8a2c137
RaymondL95 <Raymondleung95@gmail.com>
2018-06-17 13:29:52 -0400

    initial push

---
+++ public/auth.js
@@ -0,0 +1,66 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '193999104654034',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.0'
+    });
+      
+    FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+    }); 
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function statusChangeCallback(response){
+    if(response.status === 'connected'){
+      console.log("Log In Successful");
+      showHide(true); //Shows App
+      getFaceBookName();   
+    } else{
+      showHide(false);
+      console.log("Failed to Log In");
+    }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function showHide(isLoggedIn){
+  if(isLoggedIn){
+    document.getElementById('chat-app').style.display = 'block';
+    document.getElementById('header').style.display = 'none';
+  }else{
+    document.getElementById('chat-app').style.display = 'none';
+    document.getElementById('header').style.display = 'block';
+  }
+}
+
+function getFaceBookName(){
+  FB.api('/me?fields=name', function(response){
+      if(response && !response.error){
+          buildUserName(response);
+      }
+  })
+}
+function buildUserName(user){
+  handle = document.getElementById('handle'),
+  handle.placeholder=user.name;
+}
+
+function logout(){
+  FB.logout(function(response){
+    showHide(false);
+  });
+}
---
+++ public/chat.js
@@ -0,0 +1,34 @@
+var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+
+var message = document.getElementById('message'),
+	handle = document.getElementById('handle'),
+	btn = document.getElementById('send'),
+	feedback = document.getElementById('feedback');
+	output = document.getElementById('output');
+
+console.log(handle.placeholder);
+
+btn.addEventListener('click',function(){
+	if(message.value !== ""){
+		socket.emit('chat', {
+			message: message.value,
+			handle: handle.placeholder
+		});
+		message.value = "";
+	}
+});
+
+message.addEventListener('keypress',function(){
+	socket.emit('typing', handle.placeholder);
+});
+
+socket.on('chat', function(data){
+	feedback.innerHTML = "";
+	output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+	feedback.innerHTML = '<p><em>' + data.handle + 'is typing a message...</em></p>';
+});
